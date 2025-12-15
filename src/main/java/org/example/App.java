da6f64b0029114a0f38de99c2a93d2c9b12976be
Adrian Bielenik <adrian.bielenik@gmail.com>
2025-12-15 19:49:16 +0100

    init commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,9 +12,9 @@ import java.sql.Connection;
 public class App {
     public static void main(String[] args) {
 
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception ignored) {}
+//        try {
+//            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+//        } catch (Exception ignored) {}
 
         SwingUtilities.invokeLater(() -> {
             try {
